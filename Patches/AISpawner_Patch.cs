5f56090fe442c58a2393720adabd90cc74573338
RidgyTheFox ♥ <ridgy.dev@gmail.com>
2022-03-06 17:39:26 +0700

    Added patch for AI.Spawner. Now AI disabled on some maps.

---
+++ Patches/AISpawner_Patch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace BDSM.Patches
+{
+    class AISpawner_Patch
+    {
+        [HarmonyPatch(typeof(AI.Spawner), "Awake")]
+        [HarmonyPrefix]
+        public static bool Awake()
+        {
+            return false;
+        }
+    }
+}
