a110113f62073f2495ada61e9249d318c96abe81
RidgyTheFox ♥ <ridgy.dev@gmail.com>
2022-03-08 05:12:08 +0700

    Added new patch for FreeMode.GameTime.

--- Plugin.cs
+++ Plugin.cs
@@ -22,6 +22,7 @@ namespace BDSM
             _harmony.PatchAll(typeof(Patches.GarageController_Pach));
             _harmony.PatchAll(typeof(Patches.AISpawner_Patch));
             _harmony.PatchAll(typeof(Patches.TimeKeeper_Patch));
+            _harmony.PatchAll(typeof(Patches.GameTime_Patch));
 
             _harmony.PatchAll(typeof(Patches.Buses.CitaroK_Patch));
             _harmony.PatchAll(typeof(Patches.Buses.Icarus260_Patch));
