9fc22bcbe2e6d255f9fbe7e359d173b978529dff
RidgyTheFox ♥ <ridgy.dev@gmail.com>
2022-03-09 19:04:12 +0700

    Added new remote player controller class. Use this if special controller for bus not implemented yet.

---
+++ RemotePlayerControllers/RemotePlayerController_Generic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BDSM.RemotePlayerControllers
+{
+    class RemotePlayerController_Generic : RemotePlayerControllerBase
+    {
+        public void Awake()
+        {
+            SetFlyingNicknameYOffset(3.0f);
+        }
+
+        public override void OnTriggerToggleAction(string l_actionName)
+        {
+
+        }
+    }
+}
